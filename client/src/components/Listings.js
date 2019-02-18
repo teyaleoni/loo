cd87dc62dfa20f05b9e0d40ca21cfa416702559b
Teya Urena <teyaleoni@gmail.com>
2019-02-17 16:39:45 -0800

    scrolling between marker and listing works

--- client/src/components/Listings.js
+++ client/src/components/Listings.js
@@ -2,27 +2,15 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getGoogleMarkers, activeListing, getListing } from '../actions/looActions'
 import '../styles/mainmap.css'
-import { Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class Listings extends Component {
   componentDidMount() {
     getGoogleMarkers()
   }
 
-  componentWillUpdate() {
-    var node = this.refs.listings
-    this.shouldScrollBottom = node.scrollTop + node.offsetHeight === node.scrollHeight + 4
-  }
-  
-  componentDidUpdate() {
-    if (this.shouldScrollBottom) {
-        var node = this.refs.listings
-        node.scrollTop = node.scrollHeight
-    }
-  }
 
   handleMouseOver = (e, listing_id) => {
-    // console.log(this.props.id) 
     activeListing(listing_id)
   }
 
@@ -33,12 +21,14 @@ class Listings extends Component {
   render() {
 
     return (
-        <div id="roomwrap" ref="listings">
+        <div id="roomwrap" >
             <div id="room">
                 {this.props.markers.map(listing => (
                 <Link key=
                   {'listing' + listing.id} to={`/listing/${listing.place_id}`}>
-                    <div className={ this.props.hover === listing.id ? "listingBox listingBoxHover": "listingBox" } 
+                    <div id={listing.id}
+                      ref = { this.props.hover === listing.id ? "shouldScroll": "" } 
+                      className={ this.props.hover === listing.id ? "listingBox listingBoxHover": "listingBox" } 
                       onMouseOver={(e) => this.handleMouseOver(e, listing.id)}
                       onMouseOut={(e) => this.handleMouseOut(e, listing.id)} >
                         <p id="name3">{listing.name}</p>
